743667a8383156f67a0072b977b6e854db038516
SkyActually <jimmiesmith1222@gmail.com>
2024-04-23 18:49:28 -0500

    Made 1st image appear

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ class Tamagotchi {
     constructor(name, owner) {
         this.name = name;
         this.owner = owner;
-        this.mood = "Brave"
+        this.mood = "Brave";
+        this.hunger = 0;
+        this.sleepiness = 0;
+        this.boredom = 0;
+        this.age = 0;
     }
 }
 
 const myTamagotchi = new Tamagotchi("Alice", "Jimmie")
-
-// console.log(myTamagotchi.name);
-// console.log(myTamagotchi.owner);
-// console.log(myTamagotchi.mood);
 
