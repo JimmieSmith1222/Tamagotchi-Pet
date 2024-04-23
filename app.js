4d4fb07a27c965882c2e6132023b9fef5502a9cf
SkyActually <jimmiesmith1222@gmail.com>
2024-04-23 18:14:48 -0500

    Made a Tomagotchi class

---
+++ app.js
@@ -0,0 +1,14 @@
+class Tamagotchi {
+    constructor(name, owner) {
+        this.name = name;
+        this.owner = owner;
+        this.mood = "Brave"
+    }
+}
+
+// const myTamagotchi = new Tamagotchi("Alice", "Jimmie")
+
+// console.log(myTamagotchi.name);
+// console.log(myTamagotchi.owner);
+// console.log(myTamagotchi.mood);
+
